192b0baf9d5f35b0bc145d40574dcc0a9b43376f
GeetShingi <geet.shingi@gmail.com>
2018-07-31 22:15:27 +0530

    The final one

--- js/particle.js
+++
@@ -1,9 +0,0 @@
-var particles = Particles.init({
-	selector: '.background',
-  color: ['#0ff','#fff','#00f'],
-  sizeVariations: 5,
-  maxParticles : 200,
-  shape : {
-      type: "star",
-      }
-});
